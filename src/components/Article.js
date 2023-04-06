00c78aa33d80793a23b055abc985b115b1670d40
Marco Giuliani <48352400+marcogiu@users.noreply.github.com>
2023-04-06 10:22:45 +0200

    update article.js & footer.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -24,14 +24,41 @@ const Article = ({
   };
 
   return (
-    <div className="container border-b-2 border-black w-[80%] mx-auto h-fit col-span-2">
-      <p className="font-bold bg-black text-white p-2 w-fit ml-2">
-        {formatSection(section)}
-      </p>
-      <h2 className="font-bold text-3xl text-center font-franklin">{title}</h2>
-      {}
-      <p className="mx-auto text-justify font-franklin w-[80%]">{abstract}</p>
-      <p className="text-right mr-2">{formatDate(published_date)}</p>
+    // container
+    <div className="container border-b-2 border-black w-[90%] mx-auto h-fit">
+      {/* sectionName & title */}
+      <div className="section-info mt-2">
+        <Link to={`/section/${section}`}>
+          <p className="font-bold bg-black text-white p-2 w-fit ml-2 capitalize">
+            {formatSection(section)}
+          </p>
+        </Link>
+        <Link to={url} target="_blank" rel="noreferrer">
+          <h2 className="font-bold text-3xl text-center font-franklin mt-2">
+            {title}
+          </h2>
+        </Link>
+      </div>
+      {/* img */}
+      <div className="mt-2">
+        {multimedia && (
+          <img
+            className="hidden max-w-[80%] mx-auto"
+            src={multimedia[1].url}
+            alt=""
+          />
+        )}
+      </div>
+      {/* info */}
+      <div className="mt-2 flex flex-col">
+        <p className="mx-auto text-justify font-franklin w-[100%]">
+          {abstract}
+        </p>
+        <div className="flex flex-row justify-between items-center mt-2">
+          <p className="font-bold">{byline}</p>
+          <p className="font-bold mr-2">{formatDate(published_date)}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { useGlobalContext } from "../context";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const { sections, formatSection } = useGlobalContext();
+
+  const scrollToTop = () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
   return (
-    <div className="w-screen mx-auto">
-      <h2>Footer</h2>
+    <div className="flex flex-col items-center mt-2">
+      <h2 className="font-bold font-ancient text-2xl">The New York Times</h2>
+      <ul className="grid grid-cols-3 gap-x-16 gap-y-2">
+        {sections.map((section, index) => {
+          return (
+            <Link to={`/section/${section}`}>
+              <li
+                className="capitalize text-center"
+                key={index}
+                onClick={scrollToTop}>
+                {formatSection(section)}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
     </div>
   );
 };
